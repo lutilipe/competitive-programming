e725bb38eae20fdbe554a9c040ffe0c5debd0244
lutilipe <lutilipe02@hotmail.com>
2025-05-20 15:14:31 -0300

    axs

--- leetcode/google.cpp
+++ leetcode/google.cpp
@@ -25,12 +25,17 @@ G       H       I
 int main() {
     vector<vector<int>> vec = {{0,0}, {0,1}, {1,1}};
     double horizontal = .9;
-    double vertical = 1.0;
+    double vertical = 1;
 
     double chanceOfSuccess = 1;
 
     for (int i = 1; i < vec.size(); i++) {
-        int divisor = pow(2, vec[i][1]);
+        vector<int> l = vec[i-1];
+        if (vec[i][0] != l[0]) {
+            chanceOfSuccess*=(vertical/pow(2, max(vec[i][0], l[0])));
+        } else if (vec[i][1] != l[1]) {
+            chanceOfSuccess*=(horizontal/pow(2, max(vec[i][0], l[0])));
+        }
     }
 
     cout << 1 - chanceOfSuccess << endl;
